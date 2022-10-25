543ae09cd83bed38514a0954421449d632cf1ee9
Morten Søe Pedersen <22166060+mortenp1337@users.noreply.github.com>
2022-10-25 14:47:40 +0200

    fix: remove monorepo option

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 const config = {
   branches: ['main'],
-  extends: ['semantic-release-monorepo'],
+  // extends: ['semantic-release-monorepo'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
