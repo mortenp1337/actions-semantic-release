49c86b2355011c0b1846224690afd01c63fb20ad
Morten Søe Pedersen <22166060+mortenp1337@users.noreply.github.com>
2022-10-31 09:44:14 +0100

    fix: minor fix2

--- farewell-action/src/index.js
+++ farewell-action/src/index.js
@@ -1,4 +1,4 @@
 const core = require('@actions/core');
 
 const person = core.getInput('person', { required: true });
-console.log(`Subdir Warm Farewell ${person}!`);
+console.log(`minor fix Warm Farewell ${person}!`);
